77d449f99eabbe7d84f7b9f884f7cc6d4560fe2e
Shree1270 <sfghotnekar@gmail.com>
2025-11-10 22:02:43 +0530

    new

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="min-h-screen  bg-background text-foreground">
+      <p className="font-inter">Hii alll how r u?</p>
+    </div>
+  )
+}
+
+export default App
